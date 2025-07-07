132478065f6b42fe226e8f5dc552e189378e8208
Johan Sebastian Delgado Mantilla <zzaibor69@gmail.com>
2025-07-07 00:55:20 -0500

    cambios

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,10 @@
-// import Swal from "./sweetalert2";
-
+//Funcion para validar los datos de inicio de sesion
 const validarSesion = async () => {
+  //Se obtienen los datos ingresados
   const userInput = document.querySelector("#usuario");
   const passwdInput = document.querySelector("#contraseña");
 
+  //Se realiza la peticion
   const response = await fetch("http://localhost:8081/api/personal/login", {
     method: "POST",
     headers: {
@@ -15,21 +16,16 @@ const validarSesion = async () => {
     }),
   });
 
-
+  //Se obtiene la respuesta
   const data = await response.json();
 
-  const personal = await fetch("http://localhost:8081/api/personal/");
-  const personalData = await personal.json();
-  console.log(personalData);
-  
-
-  console.log(data);
-
-  if (data.code == 200) { 
+  //Se valida el inicio exitoso
+  if (data.code == 200) {
+    //Se muestra un mensaje
     await Swal.fire({
       title: "Inicio de sesión",
       html: data.message,
-      timer: 5000,
+      timer: 2000,
       timerProgressBar: true,
       didOpen: () => {
         Swal.showLoading();
@@ -39,7 +35,9 @@ const validarSesion = async () => {
     return true;
   }
 
+  //Se valida si ocurrio un error
   if (data.code > 200) {
+    //Se muestra un mensaje
     Swal.fire({
       title: "Error!",
       text: data.message,
@@ -48,21 +46,6 @@ const validarSesion = async () => {
     });
     return false;
   }
-
-  // const usuarios = await response.json();
-
-  // const usuariosFind = usuarios.find(({ usuario, contrasena }) => {
-  //   if (usuario == userInput.value && contrasena == passwdInput.value)
-  //     return this;
-  // });
-
-  // if (usuariosFind != undefined) {
-  //   console.log("True");
-  //   return true;
-  // } else {
-  //   alert("Mal");
-  //   return false;
-  // }
 };
 
 document.addEventListener("DOMContentLoaded", () => {
--- loginNuevo.js
+++
@@ -1,9 +0,0 @@
-const getPersonal = async () => {
-  const response = await fetch('http://localhost:8081/api/personal');
-  const data = await response.json();
-
-  console.log(data);
-    
-}
-
-getPersonal();
